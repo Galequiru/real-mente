458a8ef7a2fb645a892bf68f913613b04b8590db
Alice Lopes <164353335+estrelyia@users.noreply.github.com>
2025-05-26 14:45:17 -0300

    ajustes na header

--- front-end/real-mente/src/components/Header.jsx
+++ front-end/real-mente/src/components/Header.jsx
@@ -7,14 +7,14 @@ function Header() {
     return (
         <div className = "header">
            <Link to = "/">
-                <img src={logo} alt="Logo" className="logo" />
+                <img src = {logo} alt = "Logo" className = "logo" />
             </Link>  
             <br />
-            <Link to = "/quem-somos"><button>Quem somos?</button></Link> 
-            <Link to = "/escolha-seu-destino"><button>Escolha seu destino</button></Link> 
-            <Link to = "/planos-para-compra"><button>Planos para compra</button></Link> 
-            <Link to = "/como-funciona"><button>Como funciona?</button></Link> 
-            <h1>teste</h1>
+            <Link to = "/quem-somos"><button className = "botaoH">Quem somos?</button></Link> 
+            <Link to = "/escolha-seu-destino"><button className = "botaoH">Escolha seu destino</button></Link> 
+            <Link to = "/planos-para-compra"><button className = "botaoH">Planos para compra</button></Link> 
+            <Link to = "/como-funciona"><button className = "botaoH">Como funciona?</button></Link> 
+            <hr />
         </div>
     )
 }
