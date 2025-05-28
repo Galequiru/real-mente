f36005d886c1f2dfecd96fc35762fc3af8a37493
Alice Lopes <164353335+estrelyia@users.noreply.github.com>
2025-05-27 22:30:49 -0300

    iniciando tela de cadastro e login

--- front-end/real-mente/src/pages/cadastro/Cadastro.jsx
+++ front-end/real-mente/src/pages/cadastro/Cadastro.jsx
@@ -5,11 +5,52 @@ function Cadastro() {
   return (
     <div className = "containerCadLog">
         <div className = "cad"> 
-            <h1>Cadastre-se</h1>
+            <h2>Cadastre-se</h2>
+            <label className = "usuario">Usuário:</label>
+            <br />
+            <input type = "text" className = "usuarioC" required/>
+            <br />
+            <br />
+            <label className = "email">Email:</label>
+            <br />
+            <input type = "text" className = "emailC" required/>
+            <br />
+            <br />
+            <label className = "cpf">CPF:</label>
+            <br />
+            <input type = "number" className = "cpfC" required/>
+            <br />
+            <br />
+            <label className = "telefone">Telefone:</label>
+            <label className = "dataNasc">Data de Nascimento:</label>
+            <br />
+            <input type = "text" className = "telefoneC" required/>
+            
+            <input type = "date" className = "dataNascC" required/>
+            <br />
+            <br />
+            <label className = "senha">Senha:</label>
+            <input type = "password" className = "senhaC" required/>
+
+            <hr />
+            <button className = "cadastrarC">Cadastrar</button>
+
 
         </div>
 
         <div className = "login">
+          <h2>Login</h2>
+          <label className = "emailUsu">Email ou Usúario:</label>
+          <br />
+          <input type = "text" className = "emailUsuL" required/>
+          <br />
+          <br />
+           
+          <label className = "senhaL">Senha:</label>
+          <input type = "password" className = "senhaC" required/>
+
+          <hr />
+          <button className = "cadastrarC">Cadastrar</button>
 
         </div>
     </div>
