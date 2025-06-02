e9c141bd5008f82013a50ad493cf040c7e39656f
Alice Lopes <164353335+estrelyia@users.noreply.github.com>
2025-06-02 18:43:02 -0300

    mexendo no css do cadastro

--- front-end/real-mente/src/pages/cadastro/Cadastro.jsx
+++ front-end/real-mente/src/pages/cadastro/Cadastro.jsx
@@ -3,56 +3,64 @@ import './Cadastro.css'
 
 function Cadastro() {
   return (
-    <div className = "containerCadLog">
-        <div className = "cad"> 
-            <h2>Cadastre-se</h2>
-            <label className = "usuario">Usuário:</label>
+    <div className = "card">
+      <div className = "containerCadLog">
+          <div className = "cad"> 
+              <h2>CADASTRE-SE</h2>
+              <label className = "usuario">Usuário:</label>
+              <br />
+              <br />
+              <input type = "text" className = "usuarioC" required/>
+              <br />
+              <br />
+              <label className = "email">Email:</label>
+              <br />
+              <input type = "text" className = "emailC" required/>
+              <br />
+              <br />
+              <label className = "cpf">CPF:</label>
+              <br />
+              <input type = "number" className = "cpfC" required/>
+              <br />
+              <br />
+              <label className = "telefone">Telefone:</label>
+              <label className = "dataNasc">Data de Nascimento:</label>
+              <br />
+              <input type = "text" className = "telefoneC" required/>
+              
+              <input type = "date" className = "dataNascC" required/>
+              <br />
+              <br />
+              <label className = "senha">Senha:</label>
+              <input type = "password" className = "senhaC" required/>
+
+              <hr />
+              <button className = "cadastrarC">Cadastrar</button>
+
+
+          </div>
+
+          <div className = "login">
+            <h2>LOGIN</h2>
             <br />
-            <input type = "text" className = "usuarioC" required/>
             <br />
             <br />
-            <label className = "email">Email:</label>
             <br />
-            <input type = "text" className = "emailC" required/>
+            <label className = "emailUsu">Email ou Usúario:</label>
             <br />
-            <br />
-            <label className = "cpf">CPF:</label>
-            <br />
-            <input type = "number" className = "cpfC" required/>
+            <input type = "text" className = "emailUsuL" required/>
             <br />
             <br />
-            <label className = "telefone">Telefone:</label>
-            <label className = "dataNasc">Data de Nascimento:</label>
-            <br />
-            <input type = "text" className = "telefoneC" required/>
             
-            <input type = "date" className = "dataNascC" required/>
+            <label className = "senhaL">Senha:</label>
             <br />
-            <br />
-            <label className = "senha">Senha:</label>
             <input type = "password" className = "senhaC" required/>
 
             <hr />
             <button className = "cadastrarC">Cadastrar</button>
 
-
-        </div>
-
-        <div className = "login">
-          <h2>Login</h2>
-          <label className = "emailUsu">Email ou Usúario:</label>
-          <br />
-          <input type = "text" className = "emailUsuL" required/>
-          <br />
-          <br />
-           
-          <label className = "senhaL">Senha:</label>
-          <input type = "password" className = "senhaC" required/>
-
-          <hr />
-          <button className = "cadastrarC">Cadastrar</button>
-
-        </div>
+          </div>
+      </div>
     </div>
   )
 }
