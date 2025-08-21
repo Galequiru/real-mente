2e770b71ff25246898d199a98afbc7bdb64a1614
Gabriel Leonardo <glquimbayor@gmail.com>
2025-08-21 03:39:10 -0300

    pagina inicial sem quebras de linha e wrapper

--- website/src/pages/home/Home.jsx
+++ website/src/pages/home/Home.jsx
@@ -3,21 +3,20 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className = "home"> 
-          <div className = "info">
-            <br />
-            <br />
-            <div className = "infoBox"></div>
-          </div>
-          <div className = "texto">
-            <p>“Entre no conteudo, mergulhe no conhecimento”</p>
-            <br />
-            <p>Exploração interativa de conteudos escolares com realidade virtual nas escolas.
-                   Desenvolvimento de um ambiente virtual imersivo para apoio ao ensino de disciplinas 
-                   escolares com uso de óculos de realidade virtual</p>
-          </div>
-          
-    </div>
+    <>
+      <div className = "info">
+        <div className = "infoBox"></div>
+      </div>
+      <div className = "texto">
+        <p>"Entre no conteudo, mergulhe no conhecimento"</p>
+        <br />
+        <p>
+          Exploração interativa de conteudos escolares com realidade virtual nas escolas.
+          Desenvolvimento de um ambiente virtual imersivo para apoio ao ensino de disciplinas
+          escolares com uso de óculos de realidade virtual
+        </p>
+      </div>
+    </>
   )
 }
 
