f55515af642223982f2fd1823b62b8385ed89092
Gabriel Leonardo <glquimbayor@gmail.com>
2025-08-21 03:13:54 -0300

    arrumada formatação do header

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -3,30 +3,30 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 import logo from '../assets/logo.png'
 
-function Header(props) {
+function Header({ usuario }) {
     return (
-        <div className = "header">
-            <div className = "loginCadastroH">
-                {props.usuario === ""
+        <header className="header">
+            <div className="loginCadastroH">
+                {usuario === ""
                 ? <>
-                    <Link to = "/login"><button className = "botaoLogH">ENTRAR</button></Link> 
-                    <Link to = "/cadastro"><button className = "botaoCadH">CADASTRE-SE</button></Link> 
+                    <Link to="/login"><button className="botaoLogH">ENTRAR</button></Link>
+                    <Link to="/cadastro"><button className="botaoCadH">CADASTRE-SE</button></Link>
                 </>
                 : <>
-                    <h1 style={{color: "white"}}>Olá, {props.usuario}</h1>
+                    <h1 style={{color: "white"}}>Olá, {usuario}</h1>
                 </>}
             </div>
-            <Link to = "/">
-            <img src = {logo} alt = "Logo" className = "logo" />
-            </Link>  
-            <br />
-            <br />
-            <Link to = "/quem-somos"><button className = "botaoH">Quem somos?</button></Link> 
-            <Link to = "/escolha-seu-destino"><button className = "botaoH">Escolha seu destino</button></Link> 
-            <Link to = "/planos-para-compra"><button className = "botaoH">Planos para compra</button></Link> 
-            <Link to = "/como-funciona"><button className = "botaoH">Como funciona?</button></Link> 
-            <hr className = "linhaH" />
-        </div>
+            <Link to="/">
+            <img src={logo} alt="Logo" className="logo" />
+            </Link>
+            <div className='menuTelas'>
+                <Link to="/quem-somos"><button className="botaoH">Quem somos?</button></Link>
+                <Link to="/escolha-seu-destino"><button className="botaoH">Escolha seu destino</button></Link>
+                <Link to="/planos-para-compra"><button className="botaoH">Planos para compra</button></Link>
+                <Link to="/como-funciona"><button className="botaoH">Como funciona?</button></Link>
+            </div>
+            <hr className="linhaH" />
+        </header>
     )
 }
 
