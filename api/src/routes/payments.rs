fe27fdb4b881ce12faeb2908fdb7824eb162c78d
Gabriel Leonardo <glquimbayor@gmail.com>
2025-10-09 02:42:55 -0300

    refactor: the register_payment function returns whether the database was updated and needs only the payment products. removed unchecked unwrappings. create_payment now also forwards the payment server response

--- api/src/routes/payments.rs
+++ api/src/routes/payments.rs
@@ -1,18 +1,11 @@
 use reqwest::Client;
-use rocket::serde::{json::{serde_json, Json}, Deserialize, Serialize};
+use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket_db_pools::mongodb::Collection;
 
 use super::*;
 use crate::{models::{PendingPayment, User}, with_message};
 
 const URL_MP: &str = "http://mercadopago_integration:8001";
-
-#[derive(Deserialize, Serialize)]
-#[serde(crate = "rocket::serde")]
-pub struct ExternalReference {
-	pub email: String,
-	pub products: Vec<String>
-}
 
 async fn check_existing_product(
 	collection: &Collection<User>,
@@ -65,11 +58,11 @@ async fn register_payment(
 	collection: &Collection<User>,
 	mut user: User,
 	payment_id: String,
-	payment_data: ExternalReference
-) -> Result<Custom<Json<Value>>, Custom<Json<Value>>> {
+	products: Vec<String>
+) -> Result<bool, Custom<Json<Value>>> {
 	user.pendings.push(PendingPayment {
-		payment_id: payment_id,
-		products: payment_data.products
+		payment_id,
+		products
 	});
 
 	let result = collection.update_one(
@@ -80,21 +73,7 @@ async fn register_payment(
 	.await
 	.map_err(with_message!("Error updating user info"))?;
 
-	if result.modified_count != 0 {
-		Ok(Custom(Status::Ok,
-			Json(json!({
-				"status": "success",
-				"message": "User's pending list updated successfully"
-			}))
-		))
-	} else {
-		Err(Custom(Status::InternalServerError,
-			Json(json!({
-				"status": "error",
-				"message": "Error when updating users pending list"
-			}))
-		))
-	}
+	Ok(result.modified_count != 0)
 }
 
 #[derive(Deserialize, Serialize)]
@@ -142,22 +121,44 @@ pub async fn create_payment(
 		.await
 		.map_err(with_message!("Error reading payment server response"))?;
 
-	if status.is_success() {
-		register_payment(
-			&collection,
-			user,
-			body["id"].as_str().unwrap().to_string(),
-			serde_json::from_str(
-				body["external_reference"].as_str().unwrap()
-			).unwrap()
-		).await
-	} else {
+	if !status.is_success() {
 		// forward the status and message
-		Err(Custom(
+		return Err(Custom(
 			Status { code: status.into() },
 			Json(json!({
 				"status": "error",
-				"message": body["message"].as_str().unwrap()
+				"message": body["message"].as_str()
+					.unwrap_or_default()
+			}))
+		))
+	}
+	if register_payment(
+		&collection,
+		user,
+		String::from(body["id"].as_str()
+			.ok_or_else(|| Custom(Status::FailedDependency,
+				Json(json!({
+					"status": "error",
+					"message": "The payment server sent an unexpected response"
+				}))
+			))?
+		),
+		data.products.clone()
+	).await? {
+		Ok(Custom(Status::Ok,
+			Json(json!({
+				"status": "success",
+				"message": "Users pending list updated successfully",
+				"data": body
+			}))
+		))
+	} else {
+		// should not happen since the existance of the user was proven
+		// when checking existing products
+		Err(Custom(Status::NotFound,
+			Json(json!({
+				"status": "error",
+				"message": "The user is not present on the database"
 			}))
 		))
 	}
